966f340059d8086781eb7cd730fe5dd243063b20
Andrej Germič <andrej.germic@gmail.com>
2021-01-10 15:34:42 +0100

    Scroll animation fixes

--- Setup/src/animations/scrollView.js
+++ Setup/src/animations/scrollView.js
@@ -1,14 +1,20 @@
 const scrollView = () => {
-    const winHeight = window.innerHeight + 50;
+    const winHeight = window.innerHeight + 150;
     const scrollPos = window.scrollY;
     const topCover = document.querySelector('.cover_top');
     const bottomCover = document.querySelector('.cover_bottom');
-
+    
     //baseTop = 0 => -200
     //baseBottom = -100 => 100
-    
     let scrollAlignDiff = scrollPos % winHeight;
     
+    if(scrollAlignDiff > winHeight - 180 && scrollAlignDiff < winHeight - 21){
+        window.scroll(0, scrollPos + 2)
+    }
+    else if(scrollAlignDiff < 180 && scrollAlignDiff > 21){
+        window.scroll(0, scrollPos - 2)
+    }
+
     if (scrollAlignDiff > winHeight/2){
         overFlow = scrollAlignDiff % (winHeight/2)
         scrollAlignDiff = winHeight/2 - overFlow
@@ -19,7 +25,8 @@ const scrollView = () => {
     topCover.style.top = `${-200 + align}vh`
     bottomCover.style.top = `${100 - align}vh`
 
+    
 }
 
 
-setInterval(scrollView, 50)
+setInterval(scrollView, 15)
